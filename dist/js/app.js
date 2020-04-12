3c64c63e004f2fc316d8f1bf314b1bca6f71f1f5
Nandom <nandommamdam@gmail.com>
2020-04-12 03:27:40 +0100

    create campaign form

---
+++ dist/js/app.js
@@ -0,0 +1,50 @@
+const menuToggler = document.querySelector('.menu--toggler');
+const aside = document.querySelector('.aside');
+const modal = document.getElementById('formModal');
+const modalBtn = document.getElementById('modalBtn');
+const closeBtn = document.getElementById('closeBtn');
+
+// Toggle navigation for responsive view
+menuToggler.addEventListener('click', event => {
+  if (aside.classList.contains('show--sm')) {
+    aside.classList.remove('show--sm');
+  } else {
+    aside.classList.add('show--sm');
+  }
+});
+
+// Open Form Modal
+modalBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+// Close Form Modal
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+// Close Modal if out side is click
+window.addEventListener('click', event => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+// Custom input button
+document.querySelectorAll('.file-upload__btn').forEach(button => {
+  const hiddenInput = button.parentElement.querySelector('.file-upload__input');
+  const label = button.parentElement.querySelector('.file-upload__label');
+  const defaultLabelText = 'No file selected';
+
+  // Set default text for label
+  label.textContent = defaultLabelText;
+
+  button.addEventListener('click', () => {
+    hiddenInput.click();
+  });
+
+  hiddenInput.addEventListener('change', () => {
+    const filenameList = Array.from(hiddenInput.files).map(file => file.name);
+    label.textContent = filenameList.join(', ') || defaultLabelText;
+  });
+});
